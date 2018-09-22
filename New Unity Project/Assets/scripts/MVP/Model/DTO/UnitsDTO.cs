30495f1593404ef895562325de60f64b306fcc9f
seolyounji <sseolyj23@naver.com>
2018-09-22 13:58:08 +0900

    파일생성

---
+++ New Unity Project/Assets/scripts/MVP/Model/DTO/UnitsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Assets.Scripts.MVP.Model.DTO
+{
+    class UnitsDTO
+    {
+        [Serializable]
+        public class CombatPlaneDTO
+       
+    }
+}
---
+++ New Unity Project/Assets/scripts/MVP/View/Scene/BattlegroundScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.MVP.View.Scene
+{
+    class BattlegroundScene:MonoBehaviour
+    {
+    }
+}
